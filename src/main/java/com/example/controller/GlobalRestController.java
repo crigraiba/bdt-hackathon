fd109ec454487406ea0fbf1701cf03dfca035853
Cristina <crigraiba@gmail.com>
2021-06-13 11:01:59 +0200

    second commit

--- src/main/java/com/example/controller/GlobalRestController.java
+++ src/main/java/com/example/controller/GlobalRestController.java
@@ -1,10 +1,25 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.entity.User;
+
 public class GlobalRestController {
 	
-	// @PostMapping("/register")
+	@PostMapping("/register")
+	public void register(@RequestBody User user) {
+	}
 	
-	// @GetMapping("/login")
+	@GetMapping("/login")
+	public String login() {
+		return null;
+	}
 	
-	// @GetMapping("/notification")
+	@GetMapping("/notification")
+	public String notification() {
+		return null;
+	}
+	
 }
