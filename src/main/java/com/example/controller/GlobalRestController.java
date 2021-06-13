28f595cee49ab46be0837acde29bff9119b09c74
Cristina <crigraiba@gmail.com>
2021-06-13 12:40:17 +0200

    final commit

--- src/main/java/com/example/controller/GlobalRestController.java
+++ src/main/java/com/example/controller/GlobalRestController.java
@@ -8,14 +8,30 @@ import com.example.entity.User;
 
 public class GlobalRestController {
 	
+	/**
+	 * Registra al usuario.
+	 * @param user
+	 */
+	
 	@PostMapping("/register")
 	public void register(@RequestBody User user) {
+		//isEmailVerified
 	}
 	
+	/**
+	 * Realiza un inicio de sesión.
+	 * @return
+	 */
+	
 	@GetMapping("/login")
-	public String login() {
+	public String login(@RequestBody User user) {
 		return null;
 	}
+	
+	/**
+	 * Devuelve un mensaje de notificación.
+	 * @return
+	 */
 	
 	@GetMapping("/notification")
 	public String notification() {
--- src/main/java/com/example/service/PostgreSQLUserService.java
+++ src/main/java/com/example/service/PostgreSQLUserService.java
@@ -20,33 +20,35 @@ public class PostgreSQLUserService implements IUserService {
 	// Lectura:
 	@Override
 	public Optional<User> readById(String id) {
-		return null;
+		return repository.findById(id);
+	}
+	
+	@Override
+	public Optional<String> readGitHubUserIdById(String id) {
+		return repository.findGitHubUserIdById(id);
+	}
+
+	@Override
+	public Optional<String> readGitLabUserIdById(String id) {
+		return repository.findGitLabUserIdById(id);
 	}
 	
 	// Creación:
 	@Override
 	public void create(User user) {
+		repository.save(user);
 	}
 	
 	// Actualización:
 	@Override
 	public void update(User user) {
+		repository.save(user);
 	}
 	
 	// Eliminación:
 	@Override
 	public void deleteById(String id) {
-	}
-	
-	// Lectura:
-	@Override
-	public Optional<String> readGitHubUserIdById(String id) {
-		return null;
-	}
-
-	@Override
-	public Optional<String> readGitLabUserIdById(String id) {
-		return null;
+		repository.deleteById(id);
 	}
 
 }
